d37d35264da23c4512e517c02b6feee84ee8d86f
Jaap Jansma <jaap.jansma@civicoop.org>
2019-05-09 16:19:33 +0200

    fixed issue with upgrader

--- CRM/Dataprocessor/Upgrader.php
+++ CRM/Dataprocessor/Upgrader.php
@@ -7,7 +7,7 @@ use CRM_Dataprocessor_ExtensionUtil as E;
 class CRM_Dataprocessor_Upgrader extends CRM_Dataprocessor_Upgrader_Base {
 
   public function install() {
-    
+
   }
 
   public function uninstall() {
